dfecc5ac2a167012c259fcaf3a99e34601c9a529
Egor Rudkov <gren236@gmail.com>
2025-11-16 15:29:27 +0100

    add support for single quotes in args

--- src/cmd.zig
+++ src/cmd.zig
@@ -9,11 +9,16 @@ pub const BuiltIn = enum {
     cd,
 };
 
-pub fn handleExit(status: u8) void {
+pub fn handleExit(args: Args) !void {
+    const status: u8 = if (args.parsed.len != 0) try std.fmt.parseUnsigned(u8, args.parsed[0], 10) else 0;
+
     std.posix.exit(status);
 }
 
-pub fn handleType(allocator: std.mem.Allocator, writer: *std.Io.Writer, arg: []const u8) !void {
+pub fn handleType(allocator: std.mem.Allocator, writer: *std.Io.Writer, args: Args) !void {
+    if (args.parsed.len == 0) return error.BadArguments;
+
+    const arg = args.parsed[0];
     if (std.meta.stringToEnum(BuiltIn, arg) != null) {
         try writer.print("{s} is a shell builtin\n", .{arg});
         return;
@@ -31,7 +36,7 @@ pub fn handleType(allocator: std.mem.Allocator, writer: *std.Io.Writer, arg: []c
     try writer.print("{s} is {s}\n", .{ arg, full_path });
 }
 
-pub fn handleCommand(allocator: std.mem.Allocator, writer: *std.Io.Writer, command: []const u8, args_str: []const u8) !void {
+pub fn handleCommand(allocator: std.mem.Allocator, writer: *std.Io.Writer, command: []const u8, args: Args) !void {
     // check if command exists
     const path_val = try std.process.getEnvVarOwned(allocator, "PATH");
     defer allocator.free(path_val);
@@ -46,11 +51,7 @@ pub fn handleCommand(allocator: std.mem.Allocator, writer: *std.Io.Writer, comma
     defer args_list.deinit(allocator);
 
     try args_list.append(allocator, command);
-
-    var args_iter = std.mem.splitScalar(u8, args_str, ' ');
-    while (args_iter.next()) |arg| {
-        try args_list.append(allocator, arg);
-    }
+    try args_list.appendSlice(allocator, args.parsed);
 
     var cmd_proc = std.process.Child.init(args_list.items, allocator);
     _ = try cmd_proc.spawnAndWait();
@@ -79,8 +80,12 @@ fn checkCommandInPath(allocator: std.mem.Allocator, path_str: []const u8, comman
     return error.NotFound;
 }
 
-pub fn handleEcho(writer: *std.Io.Writer, arg: []const u8) !void {
-    try writer.print("{s}\n", .{arg});
+pub fn handleEcho(writer: *std.Io.Writer, args: Args) !void {
+    for (args.parsed, 0..) |arg, i| {
+        if (i != 0) try writer.print(" ", .{});
+        try writer.print("{s}", .{arg});
+    }
+    try writer.print("\n", .{});
 }
 
 pub fn handlePwd(allocator: std.mem.Allocator, writer: *std.Io.Writer) !void {
@@ -90,9 +95,16 @@ pub fn handlePwd(allocator: std.mem.Allocator, writer: *std.Io.Writer) !void {
     try writer.print("{s}\n", .{path});
 }
 
-pub fn handleCd(allocator: std.mem.Allocator, writer: *std.Io.Writer, arg: []const u8) !void {
+pub fn handleCd(allocator: std.mem.Allocator, writer: *std.Io.Writer, args: Args) !void {
     var path: []u8 = undefined;
     defer allocator.free(path);
+
+    var arg: []const u8 = undefined;
+    if (args.parsed.len == 0) {
+        arg = "~";
+    } else {
+        arg = args.parsed[0];
+    }
 
     // check if ~ passed
     if (std.mem.startsWith(u8, arg, "~")) {
@@ -109,22 +121,129 @@ pub fn handleCd(allocator: std.mem.Allocator, writer: *std.Io.Writer, arg: []con
     };
 }
 
+const Args = struct {
+    parsed: []const []const u8,
+
+    fn init(allocator: std.mem.Allocator, raw: []const u8) !Args {
+        var parsed_list = try std.ArrayList([]const u8).initCapacity(allocator, 0);
+
+        var in_quotes = false;
+        var arg_buf = try std.ArrayList(u8).initCapacity(allocator, raw.len);
+        defer arg_buf.deinit(allocator); // just in case
+        for (raw) |char| {
+            // if \ met - toggle in quotes
+            if (char == '\'') {
+                in_quotes = if (in_quotes) false else true;
+                continue;
+            }
+
+            if (char == ' ') {
+                // if whitespace and we are in quotes - add it to the arg
+                if (in_quotes) {
+                    try arg_buf.append(allocator, char);
+                    continue;
+                }
+
+                // if whitespace and not in quotes - finish this arg and reinit buffer
+                if (arg_buf.items.len > 0) {
+                    try parsed_list.append(allocator, try arg_buf.toOwnedSlice(allocator));
+                    arg_buf = try std.ArrayList(u8).initCapacity(allocator, raw.len);
+                    continue;
+                }
+
+                // not in quotes, arg has no chars - just skip
+                continue;
+            }
+
+            // otherwise just add a regular char to arg
+            try arg_buf.append(allocator, char);
+        }
+
+        // if no more chars and still in quotes - error
+        if (in_quotes) {
+            return error.BadArguments;
+        }
+
+        // check if there is a last arg not appended
+        if (arg_buf.items.len > 0) {
+            try parsed_list.append(allocator, try arg_buf.toOwnedSlice(allocator));
+        }
+
+        return .{
+            .parsed = try parsed_list.toOwnedSlice(allocator),
+        };
+    }
+
+    fn deinit(self: *Args, allocator: std.mem.Allocator) void {
+        for (self.parsed) |arg| {
+            allocator.free(arg);
+        }
+
+        allocator.free(self.parsed);
+    }
+};
+
+test Args {
+    const TestCase = struct {
+        input: []const u8,
+        expected: []const []const u8 = undefined,
+        expectErr: bool = false,
+    };
+    const test_cases = [_]TestCase{
+        .{
+            .input = "hello world",
+            .expected = &.{ "hello", "world" },
+        },
+        .{
+            .input = "hello        world",
+            .expected = &.{ "hello", "world" },
+        },
+        .{
+            .input = "'hello' 'world'",
+            .expected = &.{ "hello", "world" },
+        },
+        .{
+            .input = "'hello     world'",
+            .expected = &.{"hello     world"},
+        },
+        .{
+            .input = "hello''world",
+            .expected = &.{"helloworld"},
+        },
+        .{
+            .input = "hello'world",
+            .expectErr = true,
+        },
+    };
+
+    for (test_cases) |tc| {
+        std.debug.print("Args testcase: {s}\n", .{tc.input});
+
+        if (tc.expectErr) {
+            try std.testing.expectError(error.BadArguments, Args.init(std.testing.allocator, tc.input));
+        } else {
+            var args = try Args.init(std.testing.allocator, tc.input);
+            defer args.deinit(std.testing.allocator);
+            try std.testing.expectEqualDeep(tc.expected, args.parsed);
+        }
+    }
+}
+
 pub fn handle(allocator: std.mem.Allocator, writer: *std.Io.Writer, args_iter: *std.mem.SplitIterator(u8, .scalar)) !void {
     const command_str = args_iter.first();
     const command = std.meta.stringToEnum(BuiltIn, command_str) orelse BuiltIn.undefined;
 
     defer writer.flush() catch std.posix.exit(5);
 
-    switch (command) {
-        .exit => {
-            const status: u8 = if (args_iter.next()) |arg| try std.fmt.parseUnsigned(u8, arg, 10) else 0;
+    var args = try Args.init(allocator, args_iter.rest());
+    defer args.deinit(allocator);
 
-            handleExit(status);
-        },
-        .echo => try handleEcho(writer, args_iter.rest()),
-        .type => try handleType(allocator, writer, args_iter.rest()),
+    switch (command) {
+        .exit => try handleExit(args),
+        .echo => try handleEcho(writer, args),
+        .type => try handleType(allocator, writer, args),
         .pwd => try handlePwd(allocator, writer),
-        .cd => try handleCd(allocator, writer, args_iter.rest()),
-        else => try handleCommand(allocator, writer, command_str, args_iter.rest()),
+        .cd => try handleCd(allocator, writer, args),
+        else => try handleCommand(allocator, writer, command_str, args),
     }
 }
